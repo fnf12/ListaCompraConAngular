e30f5656fc00dc542550c9bc6ca7e13dd6c3cfc0
fnf12 <alvaroesal1@gmail.com>
2021-04-13 07:22:23 -0300

    componentes y primera subida

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from 'src/app/services/item.service';
+import { Item } from '../../models/item';
+
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items: Item[] = [];
+  total:number = 0;
+
+  constructor(private itemService:ItemService) { }
+
+  ngOnInit(): void {
+
+    this.items = this.itemService.getItems();
+
+    this.getTotal();
+  }
+
+  deleteItem(item: Item){
+    this.items = this.items.filter(x => x.id != item.id)
+    this.getTotal();
+  }
+
+  toggleItem(item: Item){
+    this.getTotal();
+  }
+
+  getTotal(){
+    this.total = this.items
+    .filter(x => !x.completed)
+    .map(x => x.quantity * x.price)
+    .reduce((acc, item) => acc += item, 0);
+  }
+
+}
